740a30ee1bd942099c857f7b58d6d92c34396104
IC-03 <isabella.callejas.m@gmail.com>
2022-08-12 23:42:27 -0500

    Cronometro css y otros cambios

--- Lista/index.js
+++ Lista/index.js
@@ -2,7 +2,6 @@ const lista = document.getElementById('lstinput');
 
 function add(){
     let texto = document.getElementById("Entradauser");
-
     var item = document.createElement('li');
     item.appendChild(document.createTextNode(texto));
     lista.appendChild(item);
