f411f06cf1f72d7dcafc6447f5eb7170da048450
IC-03 <isabella.callejas.m@gmail.com>
2022-08-13 08:27:20 -0500

    css done, sigo peleando con js

--- Lista/index.js
+++ Lista/index.js
@@ -1,14 +1,33 @@
+const lista = document.getElementById('lista');
+let ent = document.getElementById("Entradauser").innerHTML;
 
 function agregar(){
-    let ent = document.getElementById("EntradaUser");
-
-
+    var item = document.createElement('li');
+    item.appendChild(document.createTextNode(ent))
+    lista.appendChild(item)
+    typeOf(ent)
 }
+
 
 function borrar(){
-    
+
 }
 
+document.getElementById("add").onclick = agregar;
+document.getElementById("delete").onclick = borrar;
 
-document.getElementById('add').onclick = agregar;
-document.getElementById('delete').onclick = borrar;
+/*const lista = document.getElementById('lista');
+
+function add(){
+    let texto = prompt("Ingrese texto");
+    if(texto.length <= 10){
+        var item = document.createElement('li');
+        item.appendChild(document.createTextNode(texto))
+        lista.appendChild(item)
+    }
+    else{
+        console.log('No se puede textos de más de 100 letras');
+    }
+}
+
+document.getElementById('btn').onclick = add;*/
